e024825afba4434ab6fe7ea7416ba4562c033c90
ChihMin <s102062111@m102.nthu.edu.tw>
2017-11-25 18:01:45 +0800

    Complete AR object interaction

---
+++ Assets/GalleryController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class GalleryController : MonoBehaviour {
+
+	public Transform camera;
+	public Transform center;
+	// Use this for initialization
+	private Renderer renderer;
+
+	void Start() {
+		/*
+		gameObject.AddComponent<Rigidbody> ();
+		Rigidbody rb = gameObject.GetComponent<Rigidbody> ();
+		rb.useGravity = false;
+		rb.constraints = RigidbodyConstraints.FreezeRotationX;
+		rb.AddExplosionForce (100, transform.position, 30);
+		*/
+		//WWW www = new WWW ("http://1.34.198.96:3000/zelda_trim.mp4");
+		//yield return www;
+		gameObject.AddComponent<VideoPlayer> ();
+		VideoPlayer player = gameObject.GetComponent<VideoPlayer> ();
+		player.url = "http://1.34.198.96:3000/zelda_trim.mp4";
+		player.playOnAwake = true;
+		player.isLooping = true;
+		player.source = VideoSource.Url;
+		gameObject.AddComponent<Rigidbody> ();
+		Rigidbody rb = gameObject.GetComponent<Rigidbody> ();
+		rb.useGravity = false;
+		rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//gameObject.transform.RotateAround (center.position, Vector3.up, 10* Time.deltaTime);
+		//gameObject.transform.LookAt(camera);
+	}
+}
---
+++ Assets/ObjectController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour {
+
+	private Transform toDrag;
+	private float dist;
+	private Vector3 offset; 
+	private GameObject obj;
+
+	// Use this for initialization
+	void Start () {
+		//obj.AddComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount == 1 && Input.GetTouch (0).phase == TouchPhase.Began) 
+		{
+			Ray ray = Camera.main.ScreenPointToRay( Input.GetTouch(0).position );
+			RaycastHit[] hits;
+			hits = Physics.RaycastAll (ray);
+
+			for (int i = 0; i < hits.Length; ++i) {
+				RaycastHit hit = hits [i];
+				if (hit.collider.gameObject.CompareTag ("Picture")) {
+					print ("User Tap Object: " + hit.collider.gameObject.name);
+					obj = hit.collider.gameObject;
+					break;
+				}
+			}
+		}
+
+		if (obj && Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved) {
+			Touch touch = Input.GetTouch(0); // get first touch since touch count is greater than zero
+			// get the touch position from the screen touch to world point
+			Vector3 touchedPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 0.4f));
+			// lerp and set the position of the current object to that of the touch, but smoothly over time.
+			//obj.transform.position = Vector3.Lerp(obj.transform.position, touchedPos, 2 * Time.deltaTime);
+			//obj.AddComponent<Rigidbody> ();
+			Rigidbody rb = obj.GetComponent<Rigidbody> ();
+			rb.AddForce ((touchedPos - obj.transform.position) * 5.0f);
+		}
+
+		if (obj && Input.touchCount == 1 && Input.GetTouch (0).phase == TouchPhase.Ended) {
+			//Rigidbody rb = obj.GetComponent<Rigidbody> ();
+			//rb.AddForce (Vector3.zero);
+			obj = null;
+		}
+
+
+	}
+}
