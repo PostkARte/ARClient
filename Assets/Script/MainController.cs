c8f81f2c1c62a10f9a372cdb454e711346299fcd
JerryShen <jerry82422@gmail.com>
2017-12-27 11:34:00 +0800

    Update layout

--- Assets/Script/MainController.cs
+++ Assets/Script/MainController.cs
@@ -4,26 +4,27 @@ using UnityEngine;
 
 public class MainController : MonoBehaviour {
 
-	public GameObject SharedComponents;
 	public GameObject OnboardScreen;
 	public GameObject MainScreen;
 	public GameObject ReceiverScreen;
+	public GameObject IconMenu;
+	public GameObject Header;
 
 	private GameObject menu;
 
 
-	private float smoothTime = 0.3F;
+	private float smoothTime = 0.2F;
 	private Vector3 velocity = Vector3.zero;
 	private bool menuActive = false;
 
 	// Use this for initialization
 	void Start () {
-		menu = SharedComponents.transform.Find ("Menu").gameObject;
+		menu = this.transform.Find ("Menu").gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 targetPosition = new Vector3 (menuActive ? -225 : -575, 0, 0);
+		Vector3 targetPosition = new Vector3 (menuActive ? -300 : -505, 0, 0);
 		menu.transform.localPosition = Vector3.SmoothDamp(menu.transform.localPosition, targetPosition, ref velocity, smoothTime);
 	}
 
@@ -32,16 +33,20 @@ public class MainController : MonoBehaviour {
 	}
 
 	public void ChangeToMain () {
-		SharedComponents.SetActive (true);
+		IconMenu.SetActive (true);
+		Header.SetActive (true);
 		OnboardScreen.SetActive (false);
 		MainScreen.SetActive (true);
 		ReceiverScreen.SetActive (false);
 	}
 
 	public void ChangeToReceiver () {
-		SharedComponents.SetActive (true);
 		OnboardScreen.SetActive (false);
 		MainScreen.SetActive (false);
 		ReceiverScreen.SetActive (true);
+	}
+
+	public void OnSenderClick () {
+		Application.OpenURL("https://postkarte.github.io/ClientWeb");
 	}
 }
